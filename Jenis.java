30f9cfe1f0e4f2a2de27ca713fb1f7d8a2e33efc
rezanandikaa <nandikareza10@gmail.com>
2017-04-11 21:06:05 +0700

    Add files via upload

---
+++ Jenis.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Reza Nandika on 11/04/2017.
+ */
+public class Jenis {
+    int id_jenis;
+    int id_golongan;
+    String nama_jenis;
+
+    public Jenis (int id_jenis, int id_golongan, String nama_jenis)
+    {
+        this.id_jenis = id_jenis;
+        this.id_golongan = id_golongan;
+        this.nama_jenis = nama_jenis;
+    }
+
+    public void cetak()
+    {
+        System.out.println("ID JENIS : "+id_jenis);
+        System.out.println("ID Golongan : "+id_golongan);
+        System.out.println("Nama Jenis : "+nama_jenis);
+    }
+
+}
---
+++ Produk.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Reza Nandika on 11/04/2017.
+ */
+public class Produk {
+    int id_produk;
+    int id_jenis;
+    String nama_produk;
+
+    public Produk (int id_jenis, int id_produk, String nama_produk)
+    {
+       this.id_jenis = id_jenis;
+       this.id_produk = id_produk;
+       this.nama_produk = nama_produk;
+
+    }
+}
