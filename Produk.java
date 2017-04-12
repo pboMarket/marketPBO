f2ec926701f627bfc48ce5ccec184ff82a005613
yolandasham <yolanda.sham@gmail.com>
2017-04-12 22:46:58 +0700

    Update Produk.java

--- Produk.java
+++ Produk.java
@@ -11,6 +11,16 @@ public class Produk {
        this.id_jenis = id_jenis;
        this.id_produk = id_produk;
        this.nama_produk = nama_produk;
-
+    }
+    
+    public void print(){
+        System.out.println("id produk : "+id_produk);
+        System.out.println("nama_produk : "+nama_produk);
+        System.out.println("id jenis : "+id_jenis);
+    }
+    
+    public static void main(String[] args){
+        Produk produk = new Produk(1,1,"Biskuat Coklat");
+        produk.print();
     }
 }
